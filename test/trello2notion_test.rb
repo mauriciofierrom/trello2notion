5b0bb20bef4ae3fba0b80137c22f30b3665b91c2
Mauricio Fierro <mauriciofierrom@gmail.com>
2023-11-23 22:25:43 -0500

    Change wrong module naming in default test

--- test/trello2notion_test.rb
+++ test/trello2notion_test.rb
@@ -2,7 +2,7 @@
 
 require "test_helper"
 
-class Trello2notionTest < Minitest::Test
+class Trello2NotionTest < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::Trello2notion::VERSION
   end
