09fd98a091f0ac6fe1bc2f2b3217ccf86a93e4a4
Mauricio Fierro <mauriciofierrom@gmail.com>
2023-12-06 00:57:21 -0500

    Add paragraph block
    A first example of specialization of Notion blocks.

---
+++ test/trello2notion/trello/notion/paragraph_test.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+require "trello2notion/trello/notion"
+
+class ParagraphTest < Minitest::Test
+  include Trello2Notion::Notion
+
+  include BlockInterfaceTest
+  include BlockSubclassTest
+
+  def setup
+    rich_text = RichText.new(RichTextContent.new("something in the way she moves", nil), nil, "something in the way she moves")
+    @paragraph = @object = Paragraph.new(rich_text: [rich_text])
+  end
+
+  def test_to_json
+    assert_equal @paragraph.to_json, {
+      object: "object",
+      type: "paragraph",
+      paragraph: {
+        rich_text: [
+          type: "text",
+          text: {
+            content: "something in the way she moves",
+            url: nil
+          },
+          annotations: nil,
+          plain_text: "something in the way she moves"
+        ]
+      }
+    }.to_json
+  end
+end
