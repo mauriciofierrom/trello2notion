cf1dd5619179ca741db252449b8717b467ed8fa7
Mauricio Fierro <mauriciofierrom@gmail.com>
2023-12-08 16:10:32 -0500

    Add heading block
    - Add color values hash for block elements
    - Add heading block

---
+++ lib/trello2notion/trello/notion/color.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+module Trello2Notion
+  module Notion
+    COLOR = {
+      blue: "blue",
+      blue_background: "blue_background",
+      brown: "brown",
+      brown_background: "brown_background",
+      default: "default",
+      gray: "gray",
+      gray_background: "gray_background",
+      green: "green",
+      green_background: "green_background",
+      orange: "orange",
+      orange_background: "orange_background",
+      pink: "pink",
+      pink_background: "pink_background",
+      yellow: "yellow",
+      yellow_background: "yellow_background",
+      purple: "purple",
+      purple_background: "purple_background",
+      red: "red",
+      red_background: "red_background"
+    }.freeze
+  end
+end
