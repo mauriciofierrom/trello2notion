955da75385b8946559878c312c00efd93e0b4ef9
Mauricio Fierro <mauriciofierrom@gmail.com>
2023-12-08 17:17:14 -0500

    Add code block
    - Add supported programming languages in code blocks
    - Add code block

---
+++ lib/trello2notion/trello/notion/code.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+module Trello2Notion
+  module Notion
+    # A Notion code block
+    class Code < Block
+      attr_accessor :caption, :rich_text, :language
+
+      def post_init(caption: [], rich_text: [], language: LANGUAGE[:plain_text])
+        @caption = caption
+        @rich_text = rich_text
+        @language = language
+      end
+
+      def local_to_h(hash)
+        hash.merge(
+          {
+            code: {
+              caption: @caption,
+              rich_text: @rich_text,
+              language: @language
+            }
+          }
+        )
+      end
+    end
+  end
+end
