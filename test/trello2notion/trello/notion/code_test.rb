def1a2e8533ab7b37d81aa9ed318eccb9182ec5c
Mauricio Fierro <mauriciofierrom@gmail.com>
2023-12-08 17:17:14 -0500

    Add code block
    - Add supported programming languages in code blocks
    - Add code block

---
+++ test/trello2notion/trello/notion/code_test.rb
@@ -0,0 +1,47 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+require_relative "block_interface_test"
+require_relative "block_subclass_test"
+
+class CodeTest < Minitest::Test
+  include Trello2Notion::Notion
+
+  include BlockInterfaceTest
+  include BlockSubclassTest
+
+  def setup
+    caption = RichText.new(RichTextContent.new("Some caption", nil), nil, "Some caption")
+    rich_text = RichText.new(RichTextContent.new("x :: Int\nx = 12", nil), nil, "x :: Int\nx = 12")
+    @code = @object = Code.new(caption: [caption], rich_text: [rich_text], language: LANGUAGE[:haskell])
+  end
+
+  def test_to_json
+    assert_equal @code.to_json, {
+      object: "object",
+      type: "code",
+      code: {
+        caption: [
+          type: "text",
+          text: {
+            content: "Some caption",
+            url: nil
+          },
+          annotations: nil,
+          plain_text: "Some caption"
+        ],
+        rich_text: [
+          type: "text",
+          text: {
+            content: "x :: Int\nx = 12",
+            url: nil
+          },
+          annotations: nil,
+          plain_text: "x :: Int\nx = 12"
+        ],
+        language: "haskell"
+      }
+    }.to_json
+  end
+end
