9698cf44c45e1dd26022bf58db422c267eda0eb1
Mauricio Fierro <mauriciofierrom@gmail.com>
2023-12-08 17:35:47 -0500

    Add divider block

---
+++ test/trello2notion/trello/notion/divider_test.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+require_relative "block_interface_test"
+require_relative "block_subclass_test"
+
+class DividerTest < Minitest::Test
+  include Trello2Notion::Notion
+
+  include BlockInterfaceTest
+  include BlockSubclassTest
+
+  def setup
+    @divider = @object = Divider.new
+  end
+
+  def test_to_json
+    assert_equal @divider.to_json, {
+      object: "object",
+      type: "divider",
+      divider: {}
+    }.to_json
+  end
+end
