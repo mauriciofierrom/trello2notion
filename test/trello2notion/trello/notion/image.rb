d7a15a0f5f41a2e7c88a80aa14a6bd6460c69e4a
Mauricio Fierro <mauriciofierrom@gmail.com>
2023-12-11 12:11:42 -0500

    Add Image block
    - Uses files (notion and external) to indicate their source
    - They must have a valid url
    - They must have an extension of a supported Notion image type

---
+++ test/trello2notion/trello/notion/image.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+module Trello2Notion
+  module Notion
+    # A Notion Image block
+    class Image < Block
+
+    end
+  end
+end
---
+++ test/trello2notion/trello/notion/image_test.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+require_relative "block_interface_test"
+require_relative "block_subclass_test"
+
+class ImageTest < Minitest::Test
+  include Trello2Notion::Notion
+  include BlockInterfaceTest
+  include BlockSubclassTest
+
+  def setup
+    file = ExternalFile.new(url: "https://some.com/image.png")
+    @image = @object = Image.new(file:)
+  end
+
+  def test_to_json
+    assert_equal @image.to_json, {
+      object: "object",
+      type: "image",
+      image: {
+        type: "external",
+        external: {
+          url: "https://some.com/image.png"
+        }
+      }
+    }.to_json
+  end
+
+  def test_unsupported_image_type_error
+    assert_raises UnsupportedImageTypeError do
+      file = ExternalFile.new(url: "https://some.com/image.webp")
+      Image.new(file:)
+    end
+  end
+end
