c541e54de3a622517e5abe6d3c4fb60e021d0fcc
Mauricio Fierro <mauriciofierrom@gmail.com>
2024-01-16 23:07:25 -0500

    Convert strong markdown text to rich_text

---
+++ lib/trello2notion/trello/markdown/span_level/strong.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+require_relative "../convertible"
+require_relative "../../notion/rich_text"
+
+module Trello2Notion
+  module Markdown
+    # A Markdown strong text
+    class Strong
+      include Convertible
+
+      def initialize(element)
+        @element = element
+      end
+
+      def convert
+        content = RichTextContent.new(@element.value)
+        annotations = default_annotations
+        annotations.bold = true
+
+        RichText.new(content, annotations, @element.value)
+      end
+    end
+  end
+end
