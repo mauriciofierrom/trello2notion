1b5f5be8bc1ae15215b978d9d1a2e2e0beac1d4d
Mauricio Fierro <mauriciofierrom@gmail.com>
2024-01-25 05:50:43 -0500

    Convert from Kramdown::Element to a Convertible
    Due to lack of polymorphism we have to dispatch on the type of element
    to create the right Convertible

--- lib/trello2notion/trello/markdown/convertible.rb
+++ lib/trello2notion/trello/markdown/convertible.rb
@@ -6,6 +6,8 @@ module Trello2Notion
     module Convertible
       attr_accessor :element
 
+      class UnsupportedElementError < StandardError; end
+
       def extract_text
         @element
           .children
@@ -15,6 +17,40 @@ module Trello2Notion
 
       def convert
         raise NotImplementedError, "Class #{self.class} must implement method #convert"
+      end
+      def self.from_elem(element)
+        case element.type
+        when :p
+          Paragraph.new(element)
+        when :blockquote
+          BlockQuote.new(element)
+        when :codeblock
+          CodeBlock.new(element)
+        when :header
+          Header.new(element)
+        when :li
+          ListItem.new(element)
+        when :ul
+          UnorderedList.new(element)
+        when :ol
+          OrderedList.new(element)
+        when :codespan
+          CodeSpan.new(element)
+        when :em
+          Emphasis.new(element)
+        when :img
+          Image.new(element)
+        when :br
+          LineBreak.new(element)
+        when :a
+          Link.new(element)
+        when :strong
+          Strong.new(element)
+        when :text
+          Text.new(element)
+        else
+          raise UnsupportedElementError, "Element of type #{element.type} is not supported"
+        end
       end
     end
   end
--- test/trello2notion/trello/markdown/convertible_test.rb
+++ test/trello2notion/trello/markdown/convertible_test.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+require "kramdown"
 require "test_helper"
 
 require_relative "convertible_interface_test"
@@ -32,6 +33,37 @@ class ConvertibleTest < Minitest::Test
 
   def test_extract_text
     assert_equal StubbedConvertible.new.extract_text, "Test text"
+  end
+
+  def test_from_elem
+    elements = {
+      p: Trello2Notion::Markdown::Paragraph,
+      blockquote: Trello2Notion::Markdown::BlockQuote,
+      codeblock: Trello2Notion::Markdown::CodeBlock,
+      header: Trello2Notion::Markdown::Header,
+      li: Trello2Notion::Markdown::ListItem,
+      ol: Trello2Notion::Markdown::OrderedList,
+      ul: Trello2Notion::Markdown::UnorderedList,
+      codespan: Trello2Notion::Markdown::CodeSpan,
+      em: Trello2Notion::Markdown::Emphasis,
+      img: Trello2Notion::Markdown::Image,
+      br: Trello2Notion::Markdown::LineBreak,
+      a: Trello2Notion::Markdown::Link,
+      strong: Trello2Notion::Markdown::Strong,
+      text: Trello2Notion::Markdown::Text
+    }
+
+    elements.each do |e, c|
+      element = Kramdown::Element.new(e)
+      assert_instance_of c, Convertible.from_elem(element)
+    end
+  end
+
+  def test_from_elem_unsupported
+    unsupported_element = Kramdown::Element.new(:sike)
+    assert_raises(Trello2Notion::Markdown::Convertible::UnsupportedElementError) do
+      Convertible.from_elem(unsupported_element)
+    end
   end
 end
 
