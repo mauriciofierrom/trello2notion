15dfeb4cd72ae496bc22e8305e0b0a9004f06186
Mauricio Fierro <mauriciofierrom@gmail.com>
2024-03-30 12:29:06 -0500

    Add basic pretty printing for markdown and notion
    Print the trees in an easier-to-digest fashion for debugging purposes

--- lib/trello2notion.rb
+++ lib/trello2notion.rb
@@ -4,6 +4,7 @@ require_relative "trello2notion/version"
 require_relative "trello2notion/trello/notion"
 require_relative "trello2notion/trello/markdown"
 require_relative "trello2notion/trello/trello"
+require_relative "trello2notion/trello/debug"
 
 module Trello2notion
   class Error < StandardError; end
