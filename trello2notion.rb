25bd3909ced2a9f771420ab35fe654a364b9c803
Mauricio Fierro <mauriciofierrom@gmail.com>
2024-04-06 23:22:11 -0500

    Update main entry point with notion action
    - Use the NotionDecorator to generate the base page
    - Uee Notion client to create the page
    - Use id of newly-created base page to create the subpages

--- trello2notion.rb
+++ trello2notion.rb
@@ -6,6 +6,8 @@ require "json"
 
 require_relative "lib/trello2notion/trello/json_export"
 require_relative "lib/trello2notion/trello/decorators"
+require_relative "lib/trello2notion/trello/notion/client"
+require_relative "lib/trello2notion/trello/notion/parent"
 
 include Trello2Notion::Trello
 
@@ -16,6 +18,10 @@ OptionParser.new do |opts|
 
   opts.on("-m", "--markdown", "Generate markdown") do
     options[:action] = :markdown
+  end
+
+  opts.on("-n", "--notion", "Generate Notion") do
+    options[:action] = :notion
   end
 
   opts.on("-f", "--file MANDATORY", "Input file") do |f|
@@ -32,17 +38,28 @@ OptionParser.new do |opts|
   end
 end.parse!
 
+raise "No input file specified. Call trello2notion with the -f / --file flag" unless options[:file]
+input_file = File.read(options[:file])
+parsed_board = JSONExport.parse(JSON.parse(input_file))
+
 case options[:action]
 when :markdown
-  raise "No input file specified. Call trello2notion with the -f / --file flag" unless options[:file]
-
-  input_file = File.read(options[:file])
-  parsed_board = JSONExport.parse(JSON.parse(input_file))
   markdown = MarkdownDecorator.new(parsed_board).generate
   if options[:output_file]
     File.write(options[:output_file], markdown)
   else
     pp markdown
+  end
+when :notion
+  notion_client = Trello2Notion::Trello::Notion::Client.new(ENV["NOTION_API_KEY"])
+
+  base_page, subpages_builder = NotionDecorator.new(parsed_board).generate
+  base_page.parent = Trello2Notion::Notion::PageParent.new(page_id: "3c814723-fbe4-4ca8-b79c-439fed8ad1b7")
+  page_id = notion_client.create_page(base_page.to_json)
+  subpages_builder.call(page_id:).each do |subpage|
+    notion_client.create_page(subpage.to_json)
+  rescue e
+    pp subpage
   end
 else
   puts "No action provided"
