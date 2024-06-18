289a519a9d47a8da7821d6d212391f9f31dfe69d
Mauricio Fierro <mauriciofierrom@gmail.com>
2024-06-17 23:39:52 -0500

    Enable notion token middleware

--- support/rate-limiter/index.js
+++ support/rate-limiter/index.js
@@ -3,10 +3,11 @@ const functions = require("@google-cloud/functions-framework");
 const rateLimit = require("./middlewares/rate-limit");
 const auth = require("./middlewares/auth");
 const fileUpload = require("./middlewares/file-upload");
+const notionToken = require("./middlewares/notion-token");
 const runMiddlewares = require("./util/middleware-runner");
 
 functions.http("rate-limiter", async (req, res) => {
-  const middlewares = [rateLimit, auth, fileUpload];
+  const middlewares = [rateLimit, auth, fileUpload, notionToken];
 
   try {
     await runMiddlewares(middlewares, req, res);
