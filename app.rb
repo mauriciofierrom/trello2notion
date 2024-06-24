ac2190ed0654ca5f14e2e41f14ef976fde1d636e
Mauricio Fierro <mauriciofierrom@gmail.com>
2024-06-24 15:47:19 -0500

    Add notion processing to cloud function
    - Fetch Notion token from Firestore
    - Add Notion processing

--- app.rb
+++ app.rb
@@ -2,6 +2,7 @@
 
 require "base64"
 require "functions_framework"
+require "google/cloud/firestore"
 require "google/cloud/storage"
 require "json"
 require "sendgrid-ruby"
@@ -19,6 +20,7 @@ SENDER_EMAIL = "trello2notion@mauriciofierro.dev"
 include Trello2Notion::Trello
 # rubocop:enable Style/MixinUsage
 
+# rubocop:disable Metrics/BlockLength
 FunctionsFramework.cloud_event "trello2notion" do |cloud_event|
   data = JSON.parse(Base64.decode64(cloud_event.data["message"]["data"]))
   pp data
@@ -36,16 +38,28 @@ FunctionsFramework.cloud_event "trello2notion" do |cloud_event|
     markdown = MarkdownDecorator.new(parsed_board).generate
     result_bucket = storage.bucket RESULT_BUCKET_NAME, skip_lookup: true
     result_bucket.create_file StringIO.new(markdown), "#{data["email"]}_#{Time.now.to_i}.md"
-
     # Send email
     # send_email(data["email"], generated_file.url)
   when "notion"
-    # TODO: This is next after updating the frontend with the Notion oauth dance
+    notion_info = fetch_notion_info(data["email"])
+    notion_client = Trello2Notion::Trello::Notion::Client.new(notion_info["accessToken"])
+
+    base_page, subpages_builder = NotionDecorator.new(parsed_board).generate
+    base_page.parent = Trello2Notion::Notion::PageParent.new(page_id: notion_info["workspaceId"])
+    page_id = notion_client.create_page(base_page.to_json)
+    # rubocop:disable Style/RescueStandardError
+    subpages_builder.call(page_id:).each do |subpage|
+      notion_client.create_page(subpage.to_json)
+    rescue
+      pp subpage
+    end
+    # rubocop:enable Style/RescueStandardError
   else
     # Will raise 500 automatically by the functions framework
     raise "No valid method"
   end
 end
+# rubocop:enable Metrics/BlockLength
 
 # rubocop:disable Style/MixinUsage
 def send_email(email, file_url)
@@ -62,3 +76,14 @@ def send_email(email, file_url)
   sg.client.mail._("send").post(request_body: mail.to_json)
 end
 # rubocop:enable Style/MixinUsage
+
+def fetch_notion_token(email)
+  firestore = Google::Cloud::Firestore.new project_id: "trello2notion"
+
+  # Get a collection reference
+  cities_col = firestore.col "t2n-notion-token"
+
+  # Create a query
+  notion_info, * = cities_col.where(:email, :==, email).get
+  notion_info
+end
