5f7cc6de7e3bc58066162f5a82023649bb272e51
Mauricio Fierro <mauriciofierrom@gmail.com>
2024-06-25 09:53:06 -0500

    Add CORS middleware
    - Allow POST
    - Allow Content-Type header

--- support/rate-limiter/index.js
+++ support/rate-limiter/index.js
@@ -1,16 +1,20 @@
 const functions = require("@google-cloud/functions-framework");
-
 const rateLimit = require("./middlewares/rate-limit");
+const cors = require("./middlewares/cors");
 const auth = require("./middlewares/auth");
 const fileUpload = require("./middlewares/file-upload");
 const notionToken = require("./middlewares/notion-token");
 const runMiddlewares = require("./util/middleware-runner");
 
 functions.http("rate-limiter", async (req, res) => {
-  const middlewares = [rateLimit, auth, fileUpload, notionToken];
+  const middlewares = [rateLimit, cors, auth, fileUpload, notionToken];
+
+  res.set('Access-Control-Allow-Origin', '*');
 
   try {
     await runMiddlewares(middlewares, req, res);
+
+    // Fallback response
     res.status(200).send("We're done here");
   } catch (error) {
     res.status(error.status || 500).send(error.message);
